6034488729d7432d4e4d3b83d44110bf1469176b
dczarnota <dczarno@gmail.com>
2014-08-04 15:12:35 -0700

    Messages appear.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,57 @@
 // YOUR CODE HERE:
 
+var apiURL = "https://api.parse.com/1/classes/chatterbox";
+var post = function(message){
+  $.ajax({
+    url: apiURL,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+var messages;
+
+var get = function(){
+  $.ajax({
+    url: apiURL,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log("GET success");
+      messages = data.results;
+      displayMessages(messages);
+    },
+    error: function (data) {
+      console.log("GET error");
+      messages = ['GET request failed'];
+    }
+  });
+};
+
+var displayMessages = function(messages){
+  // .username
+  // .text
+  // .roomname
+  // .createdAt
+  // .updatedAd
+  // .objectID
+  var $header = $('h1');
+  for(var i = 0; i < messages.length; i++){
+    var message = messages[i];
+    var $message = $('<p></p>').insertAfter($header);
+    $message.addClass('message').text(message.text);
+    console.log(message.text);
+  }
+};
+$(document).ready(function(){
+  get();
+});
+
