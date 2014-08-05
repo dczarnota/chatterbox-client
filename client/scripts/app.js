4070101f0fea96a7570cb189fe3ec8acc5970387
dczarnota <dczarno@gmail.com>
2014-08-04 20:44:25 -0700

    user can send a message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,10 @@
 // YOUR CODE HERE:
+var refreshMessagesInterval;
+var messages;
+$(document).ready(function(){
+  app.init();
+});
+
 var post = function(message){
   $.ajax({
     url: app.server,
@@ -7,6 +13,8 @@ var post = function(message){
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      $('input').val('');
+      app.fetch();
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -14,8 +22,6 @@ var post = function(message){
     }
   });
 };
-
-var messages;
 
 var get = function(){
   $.ajax({
@@ -36,7 +42,6 @@ var get = function(){
 };
 
 var displayMessages = function(messages){
-  console.log(messages[0]);
   // .username
   // .text
   // .roomname
@@ -44,20 +49,22 @@ var displayMessages = function(messages){
   // .updatedAd
   // .objectID
   for(var i = messages.length-1; i >=0 ; i--){
+    // if($(.message).armessages[])
     app.addMessage(messages[i]);
   }
 };
+
 var addMessage = function(message){
   var $chats = $('#chats');
   var $message = $('<p></p>').prependTo($chats);
   $message.addClass('message').text(message.text);
+  $message.attr('objectId', message.objectId);
   $message.prepend('<span class="username">');
   var $username = $message.find('.username');
   $username.text(message.username);
   // debugger;
   $username.on('click', function(){
-    console.log('clicked username.');
-    app.addFriend();
+    app.addFriend(message.username);
   });
   $message.prepend('<span class="roomname">');
   var $roomname = $message.find('.roomname');
@@ -65,7 +72,7 @@ var addMessage = function(message){
   $message.append('<span class="createdAt">');
   var $createdAt = $message.find('.createdAt');
   $createdAt.text(message.createdAt);
-}
+};
 
 var refreshMessages = function(){
   app.clearMessages();
@@ -76,18 +83,31 @@ var addRoom = function(room){
   $('#roomSelect').append('<div class="room"></div>');
 };
 
-$(document).ready(function(){
-  app.init();
-});
+var init = function(){
+  $('button').on('click', function(){
+    var text = $('input').val();
+    var username = window.location.search.split('=')[1];
+    var roomname = $('#roomSelect').val();
+    var message = {username: username, text: text, roomname: roomname}
+    console.log(message);
+    app.send(message);
+  });
+
+  refreshMessagesInterval = setInterval(refreshMessages, 1000);
+};
+
+var pause = function(){
+  clearInterval(refreshMessagesInterval);
+}
 
 var app = {
-  init: function(){ refreshMessages(); },
+  init: init,
   send: post,
   server: "https://api.parse.com/1/classes/chatterbox",
   fetch: get,
   clearMessages: function(){$('#chats').children().remove();},
   addMessage: addMessage,
   addRoom: addRoom,
-  addFriend: function(){console.log("Called addFriend!");},
+  addFriend: function(username){console.log('Added',username,'as friend.');},
   handleSubmit: function(){}
 };
